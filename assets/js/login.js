2ff21af7d8d90af7de45125553f82983b0ac88bb
HeziOO <2261527538@qq.com>
2020-12-30 16:56:19 +0800

    10-登录成功后存储TOKEN并跳转到后台首页

--- assets/js/login.js
+++ assets/js/login.js
@@ -29,47 +29,45 @@ $(function () {
       // 如果判断失败 则return 一个提示消息即可
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
-        return '两次密码不一样'
+        return '两次密码不一致'
       }
     }
   })
 
-  // 注册功能 监听注册表单的提交事件
-  $('#form-reg').on('click', function (e) {
-    // 阻止默认行为
+  // 注册功能，监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
     e.preventDefault()
-    $.post(
-      'http://ajax.frontend.itheima.net/api/reguser',
-      {
-        username: $('#form-reg [name=username]').val(),
-        password: $('#form-reg [name=username]').val()
-      },
-      function (res) {
-        if (res.status !== 0) {
-          return layer.msg('注册失败', res.message)
-        }
-        layer.msg('注册成功，请登录！')
-        // 模拟人的点击行为 主动触发点击事件
-        $('#link_login').click()
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        // return console.log('注册失败', res.message);
+        return layer.msg(res.message)
       }
-    )
+      // console.log('注册成功！');
+      layer.msg('注册成功，请登录！')
+      // 主动触发点击事件
+      $('#link_login').click()
+    })
   })
 
   // 登录功能
-  $('#form-login').on('click', function (e) {
+  $('#form_login').on('submit', function (e) {
     e.preventDefault()
     console.log($(this).serialize())
     $.ajax({
-      method: 'POST',
       url: 'http://ajax.frontend.itheima.net/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
       data: $(this).serialize(),
       success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        // console.log(res.token);
+        layer.msg('登录成功')
+        // 存储
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
         location.href = '/index.html'
